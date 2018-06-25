0919e36cf2fc0ef0621c7403a43d85750b981bfe
Amir Yavand <mirmahna.s@gmail.com>
2018-06-25 15:49:58 +0430

    Add Api Routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('api\v1')->prefix('v1')->group(function () {
+    Route::apiResource('players', 'PlayerController');
+    Route::apiResource('teams', 'TeamController');
+});
