e4edb56367a2db4db49afc44e025d575588b934a
Amir Yavand <mirmahna.s@gmail.com>
2018-06-26 23:28:05 +0430

    Add Update and Destroy Methods to Endpoints

--- app/Http/Controllers/api/v1/PlayerController.php
+++ app/Http/Controllers/api/v1/PlayerController.php
@@ -19,7 +19,9 @@ class PlayerController extends Controller
         $count = \request()->has('count') ? \request('count') : 10;
         $orderBy = \request()->has('order_by') ? \request('order_by') : 'created_at';
         $sort = \request()->has('sort') ? \request('sort') : 'desc';
-        return Player::orderBy($orderBy, $sort)->paginate($count);
+        return Player::orderBy($orderBy, $sort)->with(['teams' => function ($query) {
+            $query->select('teams.id', 'teams.name', 'teams.type')->get();
+        }])->paginate($count);
     }
 
     /**
@@ -30,55 +32,63 @@ class PlayerController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'first_name' => 'required',
-            'last_name' => 'required',
-            'age' => 'required',
-            'weight' => 'required',
-            'height' => 'required',
-            'market_value' => 'required',
-            'post' => 'required|array'
-        ]);
-
+        $validator = Validator::make($request->all(), $this->validationRoles());
         if ($validator->fails())
             return response()->json(['errors' => $validator->errors(), 'status_code' => 400], 400);
 
         $player = Player::create($request->all());
-
         return response()->json(['message' => 'Player Added Successfully', 'data' => $player, 'status_code' => 201], 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int $id
+     * @param Player $player
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Player $player)
     {
-        //
+        return response()->json(['data' => $player, 'status_code' => 200], 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request $request
-     * @param  int $id
+     * @param Player $player
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Player $player)
     {
-        //
+        $player->update($request->all());
+        return response()->json(['message' => 'Player Updated Successfully', 'data' => $player, 'status_code' => 200], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int $id
+     * @param Player $player
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Player $player)
     {
-        //
+        $player->delete();
+        return response()->json([], 204);
+    }
+
+    /**
+     * @return array
+     */
+    private function validationRoles()
+    {
+        return [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'age' => 'required',
+            'weight' => 'required',
+            'height' => 'required',
+            'market_value' => 'required',
+            'post' => 'sometimes|required|array'
+        ];
     }
 }
--- app/Http/Controllers/api/v1/TeamController.php
+++ app/Http/Controllers/api/v1/TeamController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api\v1;
 use App\Team;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class TeamController extends Controller
 {
@@ -18,7 +19,7 @@ class TeamController extends Controller
         $count = \request()->has('count') ? \request('count') : 10;
         $orderBy = \request()->has('order_by') ? \request('order_by') : 'created_at';
         $sort = \request()->has('sort') ? \request('sort') : 'desc';
-        return Team::orderBy($orderBy, $sort)->paginate($count);
+        return Team::orderBy($orderBy, $sort)->with('players')->paginate($count);
     }
 
     /**
@@ -29,51 +30,59 @@ class TeamController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'type' => 'required',
-            'rank' => 'required'
-        ]);
-
+        $validator = Validator::make($request->all(), $this->validationRoles());
         if ($validator->fails())
             return response()->json(['errors' => $validator->errors(), 'status_code' => 400], 400);
 
         $team = Team::create($request->all());
-
         return response()->json(['message' => 'Team Added Successfully', 'data' => $team, 'status_code' => 201], 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param Team $team
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Team $team)
     {
-        //
+        return response()->json(['data' => $team, 'status_code' => 200], 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param Team $team
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Team $team)
     {
-        //
+        $team->update($request->all());
+        return response()->json(['message' => 'Team Updated Successfully', 'data' => $team, 'status_code' => 200], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param Team $team
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Team $team)
     {
-        //
+        $team->delete();
+        return response()->json([], 204);
+    }
+
+    /**
+     * @return array
+     */
+    private function validationRoles()
+    {
+        return [
+            'name' => 'required',
+            'type' => 'required|in:club,national',
+            'rank' => 'required'
+        ];
     }
 }
